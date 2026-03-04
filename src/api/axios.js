588c7f2deb4e2fdd74e3ce84576873df72c6af3b
abubakar000 <abubakarkhawaja74@gmail.com>
2026-03-05 02:05:35 +0500

    fix(api): change base url to liver render backend url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:5000/api';
+const BASE_URL = 'https://saasforge-backend.onrender.com/api';
 
 export default axios.create({
     baseURL: BASE_URL,
